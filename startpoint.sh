b6d5568e14633d7dedd63fa5824a72b013b61997
stc <stc@sml.at>
2018-12-03 14:49:36 +0000

    commit2

--- startpoint.sh
+++ startpoint.sh
@@ -1,4 +1,4 @@
-penssl s_client -connect ${DTR_IP}:443 -showcerts \
+openssl s_client -connect ${DTR_IP}:443 -showcerts \
     </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee \
     /usr/local/share/ca-certificates/${DTR_IP}.crt
 sudo update-ca-certificates
